4cff8e9b5221905c9fdd0ea7eec624920627a960
ahtisham225 <ahtisham.ali.jan@darkthornlabs.com>
2026-04-28 08:37:06 +0500

    Updated Homepage redirection

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,12 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
-
 export default function Home() {
-  return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+  return null
+}
 
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
-
-      <Footer />
-    </div>
-  )
+export async function getServerSideProps() {
+  return {
+    redirect: {
+      destination: 'https://q789.com.pk',
+      permanent: true,
+    },
+  }
 }
